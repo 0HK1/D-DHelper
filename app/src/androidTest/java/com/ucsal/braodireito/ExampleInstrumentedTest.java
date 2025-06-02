a3f8c0fc2d8701b6f0c453c575420c04e13497d5
0HK1 <davibeserracaldas@outlook.com>
2025-06-02 12:38:30 -0300

    feature: add case test

--- app/src/androidTest/java/com/ucsal/braodireito/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ucsal/braodireito/ExampleInstrumentedTest.java
@@ -2,13 +2,20 @@ package com.ucsal.braodireito;
 
 import android.content.Context;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import com.ucsal.braodireito.Dice.DiceActivity;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,5 +29,14 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.ucsal.braodireito", appContext.getPackageName());
+    }
+    @Rule
+    public ActivityScenarioRule<DiceActivity> activityRule =
+            new ActivityScenarioRule<>(DiceActivity.class);
+
+    @Test
+    public void turmasDevemEstarVisiveis() {
+        Espresso.onView(ViewMatchers.withId(R.id.btnHistory))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
